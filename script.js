c80d2342c90ffaa77d7dbe2e6adb00529e2e54c5
HiteshHonmane <hiteshhonmane@gmail.com>
2024-01-31 22:04:42 +0530

    Features play pause change songs
    Play pause songs feature added using JS audio. Used AudioElement from js.
    took MDN reference.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Here We go")
 
+let currentSong = new Audio();
 async function getSongs() {
     let a = await fetch("http://192.168.29.178:3000/songs/")
     let response = await a.text()
@@ -18,17 +19,26 @@ async function getSongs() {
     // console.log(songs)
 }
 
+const playMusic =(track)=>{
+    // let audio = new Audio("/songs/"+ track)
+    currentSong.src= "/songs/" + track
+    currentSong.play()
+}
+
 async function main() {
+
+    
     let songs = await getSongs()
     console.log(songs)
 
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
 
     for (const song of songs) {
+        let songname= `${song.replaceAll("%20"," ").replaceAll("[NCS Release].mp3","").replaceAll(/\([^)]*\)/g, '')} `
         songUL.innerHTML = songUL.innerHTML + `<li>
                         <img class="music-svg" src="assets/music.svg" alt="">
                         <div class="song-artist">
-                            <div class="song-name">${song.replaceAll("%20"," ").replaceAll("[NCS Release].mp3","").replaceAll(/\([^)]*\)/g, '')} 
+                            <div class="song-name" data-song ="${song}"> ${songname}
                             </div>
                             <div class="artist-name">
                             </div>
@@ -39,14 +49,25 @@ async function main() {
                         </li>`;
         
     }
-    
-    var audio = new Audio(songs[0]);
-    // audio.play();
-
-    audio.addEventListener("loadeddata",()=>{
-        let duration = audio.duration;
-        console.log(duration)
+    //linking Event listner to all songs
+    Array.from (document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click", element=>{
+        console.log(e.querySelector(".song-name").innerHTML)
+        playMusic(e.querySelector(".song-name").dataset.song)
     })
+    })
+
+    //Adding Event listner for play pause song
+    play.addEventListener("click",()=>{
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src ="assets/pause.svg" 
+        }else{
+            currentSong.pause()
+            play.src = "assets/play.svg"
+        }
+    })
+    
 
 }
 main()
